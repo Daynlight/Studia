9ae751092d6e23d23180d599444ed00d407ef989
DAYNLIGHT (✿◡‿◡) <danielstodulski000@gmail.com>
2025-05-26 02:03:13 +0200

    ELITM

---
+++ ELITM/baza_danych.pl
@@ -0,0 +1,69 @@
+/*auto(marka, kolor, rok_produkcji, przebieg_w_km)*/
+auto(opel, czerwony, 2011, 10000).
+auto(bmw, czarny, 2010, 0).
+auto(bmw, zielony, 2020,20000).
+auto(bmw, czarny, 2015,10000).
+auto(fiat, bialy, 1988,100000).
+auto(fiat, czerwony, 2000,200000).
+auto(nissan, czarny, 2014, 10000).
+auto(jaguar, srebrny, 2014, 10000).
+auto(opel, czarny, 2000, 150000).
+auto(opel, bialy, 2000, 15000).
+
+/* student(nazwisko, imiê, rok urodzenia, miejsce studiowania, kierunek studiów, uczelnia )*/
+student(kowalska, ada, 1990, poznan, informatyka,uam).
+student(kowalski, jan, 1990, poznan, matematyka,uam).
+student(kosowski, jan, 1991, poznan, matematyka, ae).
+student(kowalski, marek, 1993, poznan, matematyka, pp).
+student(kaczmarek, janina, 1989, poznan, informatyka, pp).
+student(nowak, anna, 1988, olsztyn, informatyka, uwm).
+student(jurkowski, jerzy, 1990, poznan, informatyka, uam).
+student(wolski, maciej, 1990, poznan, matematyka, pp).
+student(kot, stefan, 1989, warszawa, matematyka, uw).
+student(typowski, adam, 1990, krakow, matematyka, uj).
+student(zemski, piotr, 1991, poznan, informatyka, ue).
+student(makowska, zuzanna, 1990, poznan, informatyka, pp).
+student(ratajczak, agata, 1992, poznan, informatyka, uam).
+
+/*biblioteka(tytu³, autor, rok wydania, miejsce wydania, liczba egzemplarzy)*/
+biblioteka('Ferdydurke', 'Witold Gombrowicz', 1937, 'Warszawa',3).
+biblioteka('Quo Vadis', 'Henryk Sienkiewicz', 1896, 'Krakow',5).
+biblioteka('Potop', 'Henryk Sienkiewicz', 1898, 'Krakow',12).
+biblioteka('Ogniem i mieczem', 'Henryk Sienkiewicz', 1990, 'Krakow',34).
+biblioteka('Solaris', 'Stanislaw Lem', 1961, 'Krakow',4).
+biblioteka('Lalka', 'Boleslaw Prus', 1890, 'Warszawa',4).
+biblioteka('Pamietnik z powstania warszawskiego', 'Miron Bialoszewski', 1971, 'Warszawa',4).
+biblioteka('Chlopi', 'Wladyslaw Reymont', 1904, 'Warszawa',10).
+biblioteka('Pan Tadeusz', 'Adam Mickiewicz', 1834, 'Wilno',25).
+biblioteka('Popiol i diament', 'Jerzy Andrzejewski', 1948, 'Krakow',22).
+biblioteka('Przedwiosnie', 'Stefan Zeromski', 1924, 'Warszawa',23).
+biblioteka('Sol ziemi czarnej', 'Olga Tokarczuk', 1996, 'Krakow',1).
+biblioteka('Bieguni', 'Olga Tokarczuk', 2020, 'Krakow').
+
+/*magazyn(artyku³, cena za kilogram, rok produkcji, rok wa¿noœci, liczba kilogramów*/
+magazyn(jablka, 3.50, 2024, 2025, 100).
+magazyn(gruszki, 4.20, 2024, 2025, 50).
+magazyn(truskawki, 5.00, 2024, 2025, 40).
+magazyn(maslo, 20.50, 2024, 2025, 30).
+magazyn(ser_zolty, 32.80, 2024, 2025, 20).
+magazyn(ser_bia³y, 29.80, 2024, 2025, 60).
+magazyn(kielbasa_zywiecka, 44.50, 2024, 2025, 25).
+magazyn(herbata, 50.50, 2024, 2025, 70).
+magazyn(cukier, 6.00, 2024, 2026, 80).
+magazyn(kawa, 67.00, 2024, 2027, 45).
+magazyn(czekolada_gorzka, 51.00, 2024, 2026, 54).
+magazyn(czekolada_mleczna, 54.00, 2024, 2026, 49).
+
+/*gory(kontynent, pañstwo, masyw górski, szczyt w tym masywie, wysokoœæ szczytu)*/
+gory('Europa', 'Francja', 'Alpy', 'Mont Blanc', 4808).
+gory('Azja', 'Nepal', 'Himalaje', 'Mount Everest', 8848).
+gory('Ameryka Po³udniowa', 'Argentyna', 'Andy', 'Aconcagua', 6960).
+gory('Ameryka Po³nocna', 'Stany Zjednoczone', 'Gory Skaliste', 'Mount Elbert', 4399).
+gory('Europa', 'Szwajcaria', 'Alpy', 'Matterhorn', 4478).
+gory('Azja', 'Pakistan', 'Himalaje', 'Nanga Parbat', 8126).
+gory('Ameryka Polnocna', 'Kanada', 'Gory Skaliste', 'Mount Robson', 3954).
+gory('Europa', 'Polska', 'Karpaty', 'Rysy', 2499).
+gory('Europa', 'Polska', 'Sudety', 'Snie¿ka', 1603).
+gory('Europa', 'W³ochy', 'Alpy', 'Gran Paradiso', 4061).
+gory('Europa', 'Austria', 'Alpy', 'Grossglockner', 3798).
+gory('Europa', 'Slowacja', 'Tatry', 'Gerlach', 2655).
